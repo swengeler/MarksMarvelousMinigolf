499d9f93d44da9c135b7fe1cf649281f538a39c0
Idontknowmyname <maggiolo.matteo@gmail.com>
2016-06-14 11:56:25 +0200

    BobTheBot in progress...

--- src/physics/utils/ShotData.java
+++ src/physics/utils/ShotData.java
@@ -9,13 +9,15 @@ import entities.obstacles.Entity;
 public class ShotData {
 	
 	private Vector3f startVelocity;
+	private Vector3f startPosition;
 	private Vector3f endPosition;
 	private ArrayList<Entity> obstaclesHit;
 
-	public ShotData(Vector3f startVelocity, Vector3f endPosition, ArrayList<Entity> obstaclesHit) {
+	public ShotData(Vector3f startVelocity, Vector3f startPosition, Vector3f endPosition, ArrayList<Entity> obstaclesHit) {
 		this.startVelocity = new Vector3f(startVelocity.x, startVelocity.y, startVelocity.z);
 		this.endPosition = new Vector3f(endPosition.x, endPosition.y, endPosition.z);
 		this.obstaclesHit = (ArrayList<Entity>) obstaclesHit.clone();
+		this.startPosition = startPosition;
 	}
 	
 	public Vector3f getStartVelocity() {
@@ -28,6 +30,10 @@ public class ShotData {
 
 	public ArrayList<Entity> getObstaclesHit() {
 		return obstaclesHit;
+	}
+	
+	public Vector3f getStartPosition(){
+		return startPosition;
 	}
 	
 }
